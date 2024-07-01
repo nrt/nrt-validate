7e1bf1834c1f5b455d14cdd24e21990263314131
Loïc Dutrieux <loic.dutrieux@gmail.com>
2024-07-01 18:53:22 +0200

    Basic STACLoader (without prefetch) working

--- nrt/validate/loaders.py
+++ nrt/validate/loaders.py
@@ -12,8 +12,9 @@ TODOs/questions:
     - CRS handling, none for now. Needed? Yes, it may be needed for webmap overlay. 
 """
 from dataclasses import dataclass, field
-from typing import List, Dict, Any, Optional, Union, Callable
+from typing import List, Dict, Any, Optional, Union, Callable, TYPE_CHECKING
 from abc import ABC, abstractmethod
+import datetime
 
 import xarray as xr
 import numpy as np
@@ -22,6 +23,9 @@ from rtree.index import Index
 import rioxarray
 
 from nrt.validate import utils
+
+if TYPE_CHECKING:
+    from pystac_client import Client
 
 
 @dataclass
@@ -43,6 +47,149 @@ class BaseLoader(ABC):
 
     def __len__(self):
         return len(self.fc)
+
+
+@dataclass
+class STACLoader(BaseLoader):
+    client: 'Client'
+    collection_id: str
+    bands: List[str]
+    datetime: List[Union[datetime.datetime, str]]
+    resampling: Optional[Union[str, Dict[str, str]]]
+    vis: Dict[str, Callable[[xr.DataArray], Any]]
+    window_size: float
+    compositor: Callable[[xr.Dataset], np.ndarray]
+    query: Optional[Dict]
+    res: Optional[float] = field(default=None)
+    kwargs: dict = field(default_factory=dict)
+    """Loader to prepare data indexed into a STAC Catalogue
+
+    Args:
+        fc (list): A feature collection. Can be Points, Polygons or a mix of the two.
+            Must contain a property that can be used as a unique key
+        key (str): Name of the feature collection property to be used as unique
+            identifier
+        crs (CRS): A coordinate reference object, from fiona, rasterio, pyproj, etc
+            representing projection of both ``fc`` and ``datasets``
+        client (Client): The STAC API client.
+        collection_id (str): The STAC collection ID to query.
+        bands (list): List of bands to load from the STAC collection.
+        datetime (list): List of 2 datetime objects or strings defining the time range to query.
+        resampling (str or dict, optional): Resampling method(s) for the bands.
+        vis (dict): Dictionary of callables to compute vegetation indices
+            see ``nrt.validate.indices`` module for examples and already implemented
+            indices.
+        window_size (float): Size of the bounding box used for cropping (created around
+            the centroid of ``geom``). In CRS unit.
+        compositor (callable): Callable to transform a temporal slice of the provided
+            Dataset into a 3D numpy array. See ``nrt.validate.composites`` module
+            for examples.
+        query (dict, optional): Additional query parameters for the STAC API.
+        res (float, optional): Spatial resolution for the output data.
+        kwargs (dict, optional): Additional arguments passed to ``nrt.validate.utils.get_chips``.
+
+    Returns:
+        tuple: A tuple of 6 elements:
+            - The unique key of the sample
+            - Array of numpy.datetime64
+            - List of ipywidgets.Image (the image chips)
+            - Dictionary of values for multiple vegetation indices
+            - The sample geometry
+            - The CRS object
+
+    Examples:
+        >>> import datetime
+
+        >>> from nrt.validate.loaders import STACLoader
+        >>> from pystac_client import Client
+        >>> import planetary_computer as pc
+        >>> from pyproj import CRS
+
+        >>> from nrt.validate import utils
+        >>> from nrt.validate.indices import *
+        >>> from nrt.validate.composites import *
+        >>> from nrt.validate.xr_transforms import *
+
+
+        >>> fc = [{'geometry': {'type': 'Point', 'coordinates': (4033880, 3217980)},
+        ...        'properties': {'idx': 1}},
+        ...       {'geometry': {'type': 'Point', 'coordinates': (4395490, 3038090)},
+        ...        'properties': {'idx': 2}},
+        ...       {'geometry': {'type': 'Point', 'coordinates': (4713260, 2931020)},
+        ...                     'properties': {'idx': 3}}]
+        >>> key = 'idx'
+        >>> crs = CRS.from_epsg(3035)
+        >>> catalog = Client.open('https://planetarycomputer.microsoft.com/api/stac/v1',
+        >>>                         modifier=pc.sign_inplace)
+        >>> collection_id = 'sentinel-2-l2a'
+        >>> bands = ['B02', 'B03', 'B04', 'B08', 'B11', 'B12', 'SCL']
+        >>> resampling = {band: 'nearest' if band == 'SCL' else 'cubic' for band in bands}
+        >>> dt = [datetime.datetime(2019, 1, 1), datetime.datetime(2021,12,31)]
+        >>> vis = {'NDVI': utils.combine_transforms(S2CloudMasking(), CR_SWIR(nir='B08')),
+        >>>        'CR-SWIR': utils.combine_transforms(S2CloudMasking(), NDVI(red='B04', nir='B08'))}
+        >>> window_size = 300
+        >>> compositor = SimpleComposite(r='B04', g='B03', b='B02')
+        >>> query = {"eo:cloud_cover": {"lt": 10}}
+        >>> res = 10
+        >>> loader = STACLoader(fc=fc,
+        ...                     key=key,
+        ...                     crs=crs,
+        ...                     client=catalog,
+        ...                     collection_id=collection_id,
+        ...                     bands=bands,
+        ...                     resampling=resampling,
+        ...                     datetime=dt,
+        ...                     vis=vis,
+        ...                     window_size=window_size,
+        ...                     compositor=compositor,
+        ...                     query=query,
+        ...                     res=res)
+        ... print(loader[0])
+"""
+
+    def __post_init__(self):
+        super().__post_init__()
+        try:
+            from odc.stac import stac_load
+            from odc.geo.geobox import GeoBox
+        except ImportError:
+            raise ImportError("You must install both odc-stac and odc-geo to use STACLoader.")
+
+    def __getitem__(self, idx):
+        from odc.stac import stac_load
+        from odc.geo.geobox import GeoBox
+        feature = self.fc[idx]
+        unique_idx = feature['properties'][self.key]
+        bbox = shape(feature['geometry']).centroid.buffer(self.window_size/2).bounds
+        gbox = GeoBox.from_bbox(bbox, crs=self.crs, resolution=self.res)
+        # Query catalogue
+        query = self.client.search(collections=[self.collection_id],
+                                   bbox=gbox.geographic_extent.boundingbox.bbox,
+                                   datetime=self.datetime,
+                                   query=self.query)
+        # Load as lazy cube
+        ds = stac_load(query.items(),
+                       bands=self.bands,
+                       groupby='solar_day',
+                       chunks={'time': 1},
+                       geobox=gbox,
+                       resampling=self.resampling,
+                       fail_on_error=False)
+        # Eager load cube
+        ds = ds.compute()
+
+        # Prepare all element needed
+        dates = ds.time.values
+        values = {k:utils.get_ts(ds=ds,
+                                 geom=feature['geometry'],
+                                 vi_calculator=v)[1] for k,v in self.vis.items()}
+        chips = utils.get_chips(ds=ds,
+                                geom=feature['geometry'],
+                                size=self.window_size,
+                                compositor=self.compositor,
+                                res=self.res,
+                                **self.kwargs)
+        return unique_idx, dates, chips, values, feature['geometry'], self.crs
 
 
 @dataclass
