c1aef0796ed9fe893c2ce66ebed59cd301cd682d
Loïc Dutrieux <loic.dutrieux@gmail.com>
2024-07-05 17:49:12 +0200

    db operations in segments module now use a db connection instead of a db_path

--- nrt/validate/segments.py
+++ nrt/validate/segments.py
@@ -14,6 +14,36 @@ from traitlets import HasTraits, observe, List as TraitletsList
 
 
 class Segment(object):
+    """Represents a temporal segment with a beginning and an end, optionally labeled.
+
+    Args:
+        begin (numpy.datetime64): The beginning of the segment.
+        end (numpy.datetime64): The end of the segment.
+        label (str): An optional label for the segment
+
+    Examples:
+        >>> import numpy as np
+        >>> import sqlite3
+        >>> s = Segment(np.datetime64('2020-01-01'),
+        ...             np.datetime64('2020-01-02'),
+        ...             'forest dieback')
+        >>> print(s)
+        Temporal segment
+        begin: 2020-01-01
+        end: 2020-01-02
+        label: forest dieback
+
+        >>> s.breakpoints
+        [numpy.datetime64('2020-01-01'), numpy.datetime64('2020-01-02')]
+
+        >>> conn = sqlite3.connect(':memory:')
+        >>> s.to_db(conn, 6)
+        >>> cur = conn.cursor()
+        >>> _ = cur.execute("SELECT id, feature_id, begin, end, label FROM segments WHERE id = ?", (1,))
+        >>> row = cur.fetchone()
+        >>> print(row)
+        (1, 6, 18262, 18263, 'forest dieback')
+    """
     def __init__(self, begin: np.datetime64, end: np.datetime64, label=None):
         self.begin = begin
         self.end = end
@@ -24,22 +54,20 @@ class Segment(object):
         return [self.begin, self.end]
 
     @classmethod
-    def from_db(cls, idx, db_path):
+    def from_db(cls, idx, conn):
         """
         Create a Segment instance from the database using the segment ID.
 
         Args:
             idx (int): The ID of the segment in the database.
-            db_path (str): Path to the SQLite database.
+            conn: sqlite database connection
 
         Returns:
             Segment: An instance of the Segment class.
         """
-        conn = sqlite3.connect(db_path)
         cur = conn.cursor()
         cur.execute("SELECT id, begin, end, label FROM segments WHERE id = ?", (idx,))
         row = cur.fetchone()
-        conn.close()
         if row:
             begin = np.datetime64('1970-01-01') + np.timedelta64(row[1], 'D')
             end = np.datetime64('1970-01-01') + np.timedelta64(row[2], 'D')
@@ -47,7 +75,7 @@ class Segment(object):
         else:
             return None
 
-    def to_db(self, db_path, feature_id):
+    def to_db(self, conn, feature_id):
         """
         Save the Segment instance to the database.
 
@@ -55,7 +83,6 @@ class Segment(object):
             db_path (str): Path to the SQLite database.
             feature_id (int): The feature ID to associate with the segment.
         """
-        conn = sqlite3.connect(db_path)
         cur = conn.cursor()
         cur.execute('''CREATE TABLE IF NOT EXISTS segments
                        (id INTEGER PRIMARY KEY, begin INTEGER, end INTEGER, label TEXT, feature_id INTEGER)''')
@@ -64,7 +91,7 @@ class Segment(object):
         cur.execute("INSERT INTO segments (begin, end, label, feature_id) VALUES (?, ?, ?, ?)",
                     (begin, end, self.label, feature_id))
         conn.commit()
-        conn.close()
+        cur.close()
 
     def __str__(self):
         label = self.label if self.label else 'undefined'
@@ -75,51 +102,61 @@ class Segment(object):
 
 
 class Segmentation(HasTraits):
-    """
-    Examples:
-        >>> import os
-        >>> import tempfile
+    """Container for segmentation with observable traits
 
+    Args:
+        breakpoints (list): A list of numpy.datetime64 corresponding to breakpoints
+            around temporal segments.
+        segments (list): A list of ``Segment``s. The corresponding attribute is
+            dynamically computed and updated from the breakpoints attribute
+
+    Examples:
+        >>> import sqlite3
         >>> import numpy as np
 
-        >>> from nrt.validate.segments import Segment, Segmentation
+        >>> # Generate 50 random dates between 2005 and 2008
+        >>> start_date = np.datetime64('2005-01-01')
+        >>> end_date = np.datetime64('2008-12-31')
+        >>> num_days = (end_date - start_date).astype(int)
+        >>> random_days = np.sort(np.random.randint(0, num_days, 50))
+        >>> random_dates = start_date + random_days
+        >>> random_dates = np.append(random_dates, end_date)
+        >>> # Open a sqlite3 connection
+        >>> conn = sqlite3.connect(':memory:')
 
-        >>> # Define the start and end dates
-        >>> start_date = np.datetime64('2021-01-01')
-        >>> end_date = np.datetime64('2024-01-01')
-        >>> total_days = (end_date - start_date).astype('timedelta64[D]').astype(int)
-        >>> # Generate 20 evenly spaced days within the range
-        >>> days = np.linspace(0, total_days, num=20, dtype=int)
-        >>> dates = start_date + days
-
-        >>> # db
-        >>> temp_dir = tempfile.gettempdir()
-        >>> db_path = os.path.join(temp_dir, 'nr_test_database.sqlite')
-        >>> try:
-        ...     os.remove(db_path)
-        >>> except OSError:
-        ...     pass
-
-        >>> s1 = Segmentation.from_db_or_datelist(feature_id=12, db_path=db_path, dates=dates)
-        >>> s1.add_breakpoints(dates[5])
-        >>> s1.to_db(feature_id=12)
-        >>> s2 = Segmentation.from_db_or_datelist(feature_id=12, db_path=db_path, dates=dates)
-        >>> assert s2.breakpoints == s1.breakpoints
+        >>> seg = Segmentation.from_datelist(random_dates, conn)
+        >>> print(seg)
+        Temporal segmentation with 2 breakpoints and 1 segments
+        >>> seg.add_breakpoint(np.datetime64('2006-11-21'))
+        >>> print(seg)
+        Temporal segmentation with 3 breakpoints and 2 segments
+        >>> seg.remove_breakpoint(np.datetime64('2006-11-21'))
+        >>> print(seg)
+        Temporal segmentation with 2 breakpoints and 1 segments
+        >>> # Write results to a sqlite database
+        >>> seg.add_breakpoint(np.datetime64('2006-11-21'))
+        >>> seg.to_db(12)
+        >>> cur = conn.cursor()
+        >>> _ = cur.execute('SELECT * FROM segments WHERE feature_id = 12')
+        >>> rows = cur.fetchall()
+        >>> print(rows)
+        [(1, 12, 12788, 13473, None), (2, 12, 13473, 14244, None)]
     """
     segments: List[Segment] = TraitletsList()
     breakpoints: List[np.datetime64] = TraitletsList()
-    db_path: str
+    conn: sqlite3.Connection
 
-    def __init__(self, db_path, breakpoints=None, segments=None):
+    def __init__(self, conn, breakpoints=None, segments=None):
         super(Segmentation, self).__init__()
         self.breakpoints = breakpoints if breakpoints else []
         self.segments = segments if segments else []
-        self.db_path = db_path
+        self.conn = conn
 
     @classmethod
-    def from_datelist(cls, dates, db_path):
-        """
-        Create a Segmentation instance from a list of dates.
+    def from_datelist(cls, dates, conn):
+        """Create a Segmentation instance from a list of dates.
+
+        Assigns a single temporal segment spanning the entire time-series
 
         Args:
             dates (list of np.datetime64): List of dates.
@@ -130,12 +167,12 @@ class Segmentation(HasTraits):
         """
         begin = min(dates)
         end = max(dates)
-        instance = cls(db_path=db_path)
+        instance = cls(conn=conn)
         instance.breakpoints = [begin, end]
         return instance
 
     @classmethod
-    def from_db(cls, feature_id, db_path):
+    def from_db(cls, feature_id, conn):
         """
         Create a Segmentation instance from the database using the feature ID.
 
@@ -146,32 +183,32 @@ class Segmentation(HasTraits):
         Returns:
             Segmentation: An instance of the Segmentation class.
         """
-        segments_idx = cls.get_db_idx(feature_id, db_path)
-        segments = [Segment.from_db(idx, db_path=db_path) for idx in segments_idx]
+        segments_idx = cls.get_db_idx(feature_id, conn)
+        segments = [Segment.from_db(idx, conn=conn) for idx in segments_idx]
         breakpoints = cls.compute_breakpoints(segments)
-        instance = cls(db_path=db_path, breakpoints=breakpoints, segments=segments)
+        instance = cls(conn=conn, breakpoints=breakpoints, segments=segments)
         return instance
 
     @classmethod
-    def from_db_or_datelist(cls, feature_id, db_path, dates):
+    def from_db_or_datelist(cls, feature_id, conn, dates):
         """
         Create a Segmentation instance either from the database or from a list of dates.
 
         Args:
             feature_id (int): The feature ID.
-            db_path (str): Path to the SQLite database.
+            conn (sqlite3.Connection): Connection to a sqlite database
             dates (list of np.datetime64): List of dates.
 
         Returns:
             Segmentation: An instance of the Segmentation class.
         """
-        if cls.exists(feature_id, db_path):
-            return cls.from_db(feature_id, db_path)
+        if cls.exists(feature_id, conn):
+            return cls.from_db(feature_id, conn)
         else:
-            return cls.from_datelist(dates, db_path)
+            return cls.from_datelist(dates, conn)
 
     @staticmethod
-    def exists(feature_id, db_path):
+    def exists(feature_id, conn):
         """Check if a feature ID exists in the segments table.
 
         Args:
@@ -182,42 +219,37 @@ class Segmentation(HasTraits):
             bool: True if the feature ID exists, False otherwise.
         """
         try:
-            conn = sqlite3.connect(db_path)
             cur = conn.cursor()
             cur.execute('SELECT 1 FROM segments WHERE feature_id = ?', (feature_id,))
             result = cur.fetchone()
         except sqlite3.OperationalError as e:
             if "no such table" in str(e):
-                print(f"Error: The table does not exist in the database {db_path}.")
+                print("Error: The table does not exist in the database")
             else:
                 print(f"OperationalError: {e}")
             return False
         except sqlite3.Error as e:
             print(f"Database error: {e}")
             return False
-        finally:
-            if 'conn' in locals():
-                conn.close()
         return result is not None
 
     @staticmethod
-    def get_db_idx(feature_id, db_path):
+    def get_db_idx(feature_id, conn):
         """
         Retrieve the IDs of segments with a given feature ID.
 
         Args:
             feature_id (int): The feature ID.
-            db_path (str): Path to the SQLite database.
+            conn (sqlite3.Connection): Database connection
 
         Returns:
             list of int: List of segment IDs.
         """
-        conn = sqlite3.connect(db_path)
         cur = conn.cursor()
         cur.execute('SELECT id FROM segments WHERE feature_id = ?', (feature_id,))
         rows = cur.fetchall()
+        cur.close()
         idx = [row[0] for row in rows]
-        conn.close()
         return idx
 
     def to_db(self, feature_id):
@@ -227,23 +259,19 @@ class Segmentation(HasTraits):
             feature_id (int): The feature ID.
         """
         try:
-            conn = sqlite3.connect(self.db_path)
-            cur = conn.cursor()
+            cur = self.conn.cursor()
             cur.execute('DELETE FROM segments WHERE feature_id = ?', (feature_id,))
             conn.commit()
         except sqlite3.OperationalError as e:
             if "no such table" in str(e):
-                print(f"Error: The table does not exist in the database {self.db_path}.")
+                print("Error: The table does not exist in the database")
             else:
                 print(f"OperationalError: {e}")
         except sqlite3.Error as e:
             print(f"Database error: {e}")
-        finally:
-            if 'conn' in locals():
-                conn.close()
-        [s.to_db(self.db_path, feature_id) for s in self.segments]
+        [s.to_db(self.conn, feature_id) for s in self.segments]
 
-    def add_breakpoints(self, date):
+    def add_breakpoint(self, date):
         """
         Add a breakpoint date to the segmentation.
 
@@ -254,7 +282,7 @@ class Segmentation(HasTraits):
         bisect.insort(bp, date)
         self.breakpoints = bp
 
-    def remove_breakpoints(self, date):
+    def remove_breakpoint(self, date):
         """
         Remove a breakpoint date from the segmentation.
 
@@ -269,6 +297,16 @@ class Segmentation(HasTraits):
             bp.remove(date)
         else:
             raise ValueError('Not a valid breakpoint date')
+        self.breakpoints = bp
+
+    def add_or_remove_breakpoint(self, date):
+        """If the date provided is already a breakpoint, remove it, otherwise add it
+        """
+        bp = copy.deepcopy(self.breakpoints)
+        if date in bp:
+            bp.remove(date)
+        else:
+            bisect.insort(bp, date)
         self.breakpoints = bp
 
     @staticmethod
@@ -298,3 +336,11 @@ class Segmentation(HasTraits):
         self.segments = [Segment(begin, end) for begin, end in
                          zip(self.breakpoints[:-1], self.breakpoints[1:])]
 
+    def __str__(self):
+        message = 'Temporal segmentation with {n} breakpoints and {nn} segments'.format(n=len(self.breakpoints), nn=len(self.segments))
+        return message
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
