de07d000c5c5130e06e7570152dcd2e2a88d5bae
Loïc Dutrieux <loic.dutrieux@gmail.com>
2024-08-01 17:39:09 +0200

    New logo added to doc

--- docs/conf.py
+++ docs/conf.py
@@ -30,3 +30,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_logo = "_static/logo.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+    'style_nav_header_background': "#f8efc8"
+}
