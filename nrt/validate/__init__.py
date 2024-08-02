4989c1c8219a7c26a53b26730961761b8f04d272
Loïc Dutrieux <loic.dutrieux@gmail.com>
2024-08-02 19:57:42 +0200

    Add demo notebook to package (to be refined with data loaded from nrt.data package)

--- nrt/validate/__init__.py
+++ nrt/validate/__init__.py
@@ -1,1 +1,11 @@
+import os
+
+
 __version__ = "0.0.1"
+
+
+class DemoNotebook:
+    @staticmethod
+    def path():
+        return os.path.join(os.path.dirname(__file__), 'notebooks', 'demo_notebook.ipynb')
+
